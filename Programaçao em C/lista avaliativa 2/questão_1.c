f008b8c9422a3f9e09ec3fb352e2de36aad383d6
alv33s <felipao.muniz@gmail.com>
2023-11-05 22:43:21 -0300

    commit primeira quesão

---
+++ Programaçao em C/lista avaliativa 2/questão_1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(){
+    int valoresinicias[10];
+    int ultimovalor = 0;
+    int i;
+    int somas[10];
+    int inicio = 9;
+    int soma = 9;
+    
+
+    for(i= 0;  i < 10; i++){
+        scanf("%d", &valoresinicias[i]);
+    }
+    for(i= 0;  i < 10; i++){
+        printf("%d ", valoresinicias[i]);
+    }
+    
+    printf("\n");
+
+    while(ultimovalor == 0){
+        while(inicio != 0){
+            for(int j = 0 ; j < inicio; j++){
+                somas[j] = valoresinicias[j] + valoresinicias[j + 1];
+                printf("%d ", somas[j]);
+                for(int k = 0; k <= j ; k++ ){
+                 valoresinicias[k] = somas[k];
+                    soma--;
+                }
+                
+            }
+            printf("\n");
+            inicio--;
+        }
+        ultimovalor = 1;
+        
+    }
+    return 0;
+}
